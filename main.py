e15ea9b1a23602a4643d32011ca973e6b2f0cccd
Diniska <pavliuk.denys2018@gmail.com>
2023-04-19 16:14:25 +0300

    база

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-import colorama
-print(dir(colorama))
+import sqlite3
+
+connection = sqlite3.connect("itstep_DB.sl3", 5)
+cur = connection.cursor()
+cur.execute("INSERT INTO first_table (name) VALUES ('13');")
+connection.commit()
+connection.close()
